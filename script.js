85d8e8cd8e77132cd2934d05f91ec3ca9307435e
Surendra Kumar <surendrakeerbaniya@gmail.com>
2024-02-17 00:12:04 +0530

    page5 completed

--- script.js
+++ script.js
@@ -1,47 +1,47 @@
 function loco() {
     gsap.registerPlugin(ScrollTrigger);
 
-// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+    // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
 
-const locoScroll = new LocomotiveScroll({
-  el: document.querySelector(".main"),
-  smooth: true
-});
-// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
-locoScroll.on("scroll", ScrollTrigger.update);
+    const locoScroll = new LocomotiveScroll({
+        el: document.querySelector(".main"),
+        smooth: true
+    });
+    // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+    locoScroll.on("scroll", ScrollTrigger.update);
 
-// tell ScrollTrigger to use these proxy methods for the ".main" element since Locomotive Scroll is hijacking things
-ScrollTrigger.scrollerProxy(".main", {
-  scrollTop(value) {
-    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
-  getBoundingClientRect() {
-    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
-  },
-  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-  pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
-});
+    // tell ScrollTrigger to use these proxy methods for the ".main" element since Locomotive Scroll is hijacking things
+    ScrollTrigger.scrollerProxy(".main", {
+        scrollTop(value) {
+            return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+        }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+        getBoundingClientRect() {
+            return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+        },
+        // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+        pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
+    });
 
-// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
-ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+    // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 
-// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
-ScrollTrigger.refresh();
+    // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+    ScrollTrigger.refresh();
 
 }
 loco()
 
 function video() {
-    gsap.to(".page1 video",{
-        filter:"blur(20px)",
-        transform:"scaleX(0.95)",
-        scrollTrigger:{
-            trigger:".page1",
-            scroller:".main",
-           // markers:true,
-            start:"top 0",
-            end:"top -50%",
-            scrub:true,
+    gsap.to(".page1 video", {
+        filter: "blur(20px)",
+        transform: "scaleX(0.95)",
+        scrollTrigger: {
+            trigger: ".page1",
+            scroller: ".main",
+            // markers:true,
+            start: "top 0",
+            end: "top -50%",
+            scrub: true,
         }
     })
 }
@@ -49,54 +49,54 @@ video()
 
 // makking a cursor
 function mouse() {
-    document.addEventListener("mousemove", function(e) {
-        gsap.to(".cursor",{
-            top:e.y,
-            left:e.x
+    document.addEventListener("mousemove", function (e) {
+        gsap.to(".cursor", {
+            top: e.y,
+            left: e.x
         })
     })
-    document.querySelector(".nav i").addEventListener("mouseenter",function(){
-        gsap.to(".cursor",{
-            scale:2,
+    document.querySelector(".nav i").addEventListener("mouseenter", function () {
+        gsap.to(".cursor", {
+            scale: 2,
         })
     })
-    document.querySelector(".nav i").addEventListener("mouseleave",function(){
-        gsap.to(".cursor",{
-            scale:1,
+    document.querySelector(".nav i").addEventListener("mouseleave", function () {
+        gsap.to(".cursor", {
+            scale: 1,
         })
     })
 }
 mouse()
 
 // nav h4 animation
-function nav_h4(){
-    gsap.to(".nav-part-2",{
-        y:-100,
-        duration:1,
-        scrollTrigger:{
-            trigger:".nav",
-            scroller:".main",
-            start:"top 0",
-            end:"top -10%",
-            scrub:true,
+function nav_h4() {
+    gsap.to(".nav-part-2", {
+        y: -100,
+        duration: 1,
+        scrollTrigger: {
+            trigger: ".nav",
+            scroller: ".main",
+            start: "top 0",
+            end: "top -10%",
+            scrub: true,
         }
     })
-    
+
 }
 nav_h4()
 //nav icon
 
-function nav_i(){
-    gsap.to(".nav i",{
+function nav_i() {
+    gsap.to(".nav i", {
 
-        display:"block",
-        scrollTrigger:{
-            trigger:".nav",
-            scroller:".main",
-            start:"top -15%",
-            end:"top -20%",
-            scrub:true
-    
+        display: "block",
+        scrollTrigger: {
+            trigger: ".nav",
+            scroller: ".main",
+            start: "top -15%",
+            end: "top -20%",
+            scrub: true
+
         }
     })
 }
@@ -104,51 +104,51 @@ nav_i()
 
 // ------------------- Page2 Content Animation ---------------------- //
 
-function page2_img_scroll(){
-    gsap.to(".page2 img",{
-        transform:"translateY(-50%) translateX(53%)",
-        duration:10,
-        repeat:-1,
-        ease:"none"
+function page2_img_scroll() {
+    gsap.to(".page2 img", {
+        transform: "translateY(-50%) translateX(53%)",
+        duration: 10,
+        repeat: -1,
+        ease: "none"
     })
 }
 page2_img_scroll()
 
 function page2_animation() {
     var tl2 = gsap.timeline({
-        scrollTrigger:{
-            trigger:".page2-content",
-            scroller:".main",
+        scrollTrigger: {
+            trigger: ".page2-content",
+            scroller: ".main",
             //markers:true,
-            start:"top 90%",
-            end:"top 20%",
-            scrub:true,
+            start: "top 70%",
+            end: "top 0%",
+            scrub: true,
         }
     })
-    
-    tl2.from(".right-page2 h1",{
-        y:60,
-        duration:1,
-        opacity:0,
-        scale:1.1,
+
+    tl2.from(".right-page2 h1", {
+        y: 60,
+        duration: 1,
+        opacity: 0,
+        scale: 1.1,
     })
-    tl2.from(".left-page2",{
-        y:60,
-        duration:1,
-        opacity:0,
-        scale:1.1,
+    tl2.from(".left-page2", {
+        y: 60,
+        duration: 1,
+        opacity: 0,
+        scale: 1.1,
     })
-    tl2.from(".right-page2 p",{
-        y:60,
-        duration:2,
-        opacity:0,
-        scale:1.1,
+    tl2.from(".right-page2 p", {
+        y: 60,
+        duration: 2,
+        opacity: 0,
+        scale: 1.1,
     })
-    tl2.from(".right-page2 button",{
-        y:60,
-        duration:2,
-        opacity:0,
-        scale:1.1,
+    tl2.from(".right-page2 button", {
+        y: 60,
+        duration: 2,
+        opacity: 0,
+        scale: 1.1,
     })
 }
 page2_animation()
@@ -157,55 +157,206 @@ page2_animation()
 
 function page3_animation() {
     var tl3 = gsap.timeline({
-        scrollTrigger:{
-            trigger:".page3-content",
-            scroller:".main",
+        scrollTrigger: {
+            trigger: ".page3-content",
+            scroller: ".main",
             //markers:true,
-            start:"top 60%",
-            end:"top 20%",
-            scrub:true,
+            start: "top 60%",
+            end: "top 0%",
+            scrub: true,
         }
     })
-    
-    tl3.from(".left-page3 h1",{
-        y:60,
-        duration:1,
-        opacity:0,
-        scale:1.1,
+
+    tl3.from(".left-page3 h1", {
+        y: 60,
+        duration: 1,
+        opacity: 0,
+        scale: 1.1,
     })
-    tl3.from(".right-page3 img",{
-        y:60,
-        duration:2,
-        opacity:0,
-        scale:1.1,
+    tl3.from(".right-page3 img", {
+        y: 60,
+        duration: 2,
+        opacity: 0,
+        scale: 1.1,
     })
-    tl3.from(".left-page3 p",{
-        y:60,
-        duration:2,
-        opacity:0,
-        scale:1.1,
+    tl3.from(".left-page3 p", {
+        y: 60,
+        duration: 2,
+        opacity: 0,
+        scale: 1.1,
     })
-    tl3.from(".left-page3 button",{
-        y:60,
-        duration:2,
-        opacity:0,
-        scale:1.1,
+    tl3.from(".left-page3 button", {
+        y: 60,
+        duration: 2,
+        opacity: 0,
+        scale: 1.1,
     })
 }
 page3_animation()
 
 // ------------------- Page4 Content Animation ---------------------- //
 
-gsap.from(".page4 .helmet",{
-    y:50,
-    opacity:0,
-    scale:0.1,
-    scrollTrigger:{
-        trigger:".helmet",
-        scroller:".main",
-        markers:true,
-        start:"top 120%",
-        end:"top 80%",
-        scrub:true
+gsap.from(".page4 .helmet", {
+    y: 50,
+    opacity: 0,
+    scale: 0.1,
+    scrollTrigger: {
+        trigger: ".helmet",
+        scroller: ".main",
+       // markers: true,
+        start: "top 120%",
+        end: "top 80%",
+        scrub: true
     }
 })
+
+function p4_con1_Animation() {
+    var tl4 = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".page4-content1",
+            scroller: ".main",
+            start: "top 70%",
+            end: "top 0%",
+            scrub: true
+        }
+    })
+
+    tl4.from(".page4-content1 h2", {
+        y: 80,
+        opacity: 0,
+        scale: 1.15,
+    })
+    tl4.from(".page4-content1 p", {
+        y: 80,
+        opacity: 0,
+        scale: 1.15,
+    })
+    tl4.from(".page4-content1 button", {
+        y: 80,
+        opacity: 0,
+        scale: 1.15,
+    })
+}
+p4_con1_Animation()
+
+function p4_con2_Animation() {
+    var tl4 = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".page4-content2",
+            scroller: ".main",
+            start: "top 70%",
+            end: "top 0%",
+            scrub: true
+        }
+    })
+    tl4.from(".page4-content2 h5", {
+        y: 80,
+        opacity: 0,
+        scale: 1.15,
+    }, "con2")
+    tl4.from(".page4-content2 video", {
+        y: 80,
+        opacity: 0,
+        scale: 1.15,
+    }, "con2")
+}
+p4_con2_Animation()
+
+function p4_con3_Animation() {
+    var tl4 = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".page4-content3",
+            scroller: ".main",
+            start: "top 70%",
+            end: "top 0%",
+            scrub: true
+        }
+    })
+    tl4.from(".page4-content3 h5", {
+        y: 80,
+        opacity: 0,
+        scale: 1.15,
+    }, "con3")
+    tl4.from(".page4-content3 video", {
+        y: 80,
+        opacity: 0,
+        scale: 1.15,
+    }, "con3")
+    tl4.from(".page4-content3 button", {
+        y: 80,
+        opacity: 0,
+        scale: 1.15,
+    })  
+}
+p4_con3_Animation()
+
+function p4_con4_Animation() {
+    var tl4 = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".page4-content4",
+            scroller: ".main",
+            start: "top 70%",
+            end: "top 0%",
+            scrub: true
+        }
+    })
+    tl4.from(".page4-content4 h5", {
+        y: 80,
+        opacity: 0,
+        scale: 1.15,
+    }, "con2")
+    tl4.from(".page4-content4 video", {
+        y: 80,
+        opacity: 0,
+        scale: 1.15,
+    }, "con2")
+}
+p4_con4_Animation()
+
+function p4_con5_Animation() {
+    var tl4 = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".page4-content5",
+            scroller: ".main",
+            start: "top 70%",
+            end: "top 0%",
+            scrub: true
+        }
+    })
+    tl4.from(".page4-content5 h5", {
+        y: 80,
+        opacity: 0,
+        scale: 1.15,
+    }, "con3")
+    tl4.from(".page4-content5 video", {
+        y: 80,
+        opacity: 0,
+        scale: 1.15,
+    }, "con3")
+    tl4.from(".page4-content5 button", {
+        y: 80,
+        opacity: 0,
+        scale: 1.15,
+    })  
+}
+p4_con5_Animation()
+
+// ------------------- Page5 Content Animation ---------------------- //
+
+var tl5 = gsap.timeline({
+    scrollTrigger:{
+        trigger:".page5",
+        scroller:".main",
+        start:"top 0",
+        end:"top -80%",
+        scrub:3,
+        pin:true
+    }
+
+})
+tl5.to(".page5-content",{
+    transform:"translateX(-70%)",
+},"p5-con")
+tl5.to(".slider-in",{
+    x:670
+},"p5-con")
